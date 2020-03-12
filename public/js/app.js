fc01474b25e4d0707c029303f240d2e33092e01b
= <=>
2020-03-12 04:20:53 +0100

    performed some heroku config

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector('form');
+
 const messageOne = document.querySelector('#message-1');
+
 const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', e => {
